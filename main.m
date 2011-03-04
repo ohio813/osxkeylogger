351db2d9f56069185962032375ad2ae73d33a532
jko <jhkoivis@gmail.com>
2011-03-05 01:33:21 +0200

    Added modifier keys (output needs polishing)

--- main.m
+++ main.m
@@ -7,7 +7,7 @@
 //
 
 #import <ApplicationServices/ApplicationServices.h>
-#import <mach/mach_time.h>
+//#import <mach/mach_time.h>
 #import <Carbon/Carbon.h>
 
 
@@ -60,45 +60,76 @@ FILE* openLogFile()
 	return pLogFile;
 }
 
+void printToLog(CGEventTimestamp*	pTimeStamp,
+				uint*				pType,
+				long long*			pKeycode,
+				UniChar*			pUc,
+				UniCharCount*		pUcc,
+				CGEventFlags*		pFlags,
+				FILE*				pLogFile)
+{
+	// print key open
+	fprintf(pLogFile, "<key>");
+	// print time
+	fprintf(pLogFile, "<time>%llu</time>", *pTimeStamp);
+	// print type
+	fprintf(pLogFile, "<type>%u</type>", *pType);
+	// print keycode
+	fprintf(pLogFile, "<keycode>%lld</keycode>", *pKeycode);
+	// print unichar
+	if (pUc[0] != 0)
+		fprintf(pLogFile, "<unichar>%04x</unichar>", pUc[0]);
+	// print flags
+	fprintf(pLogFile, "<flags>%llu</flags>", *pFlags);
+	// print ascii (if alphanum)
+	if ((pUc[0] < 128) && (pUc[0] >= 41))
+		fprintf(pLogFile, "<ascii>%c</ascii>", pUc[0]);
+	// print key close
+	fprintf(pLogFile, "</key>\n");
+	
+}
+
 CGEventRef recordKeysCallback(CGEventTapProxy	proxy, 
 							  CGEventType		type,  
 							  CGEventRef		event, 
 							  void*				pLogFile) 
 {
-	// only key up and key down
+	/*
+	 if (type == kCGEventFlagsChanged)
+	{
+		printf("flags changed\n");
+		long long keycode;
+		keycode = CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+		printf("%lld", keycode);
+
+	}
+	*/
+	 
+	// only key up, key down and flagsChanged -events 
 	// you can do this also with mask
+	// flagsChanged is for modifierkeys
 	if (type != kCGEventKeyDown)
 	{
-		if (type != kCGEventKeyUp) return event;
+		if (type != kCGEventKeyUp) 
+		{
+			if (type != kCGEventFlagsChanged) return event;
+		}
 	}
 	
-	unsigned long long	currentTime; 
+	//unsigned long long	currentTime; 
+	CGEventTimestamp	timeStamp;	// unsigned long long
 	long long			keycode;
 	UniChar				uc[10];
 	UniCharCount		ucc;
+	CGEventFlags		flags;		// unsigned long long
 	
-	currentTime = mach_absolute_time();
+	//currentTime = mach_absolute_time();
+	timeStamp = CGEventGetTimestamp(event);
 	keycode = CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
 	CGEventKeyboardGetUnicodeString(event,10,&ucc,uc);
+	flags = CGEventGetFlags (event);
 	
-	if ((uc[0] < 128) && (uc[0] >= 41)){
-			fprintf((FILE*)pLogFile, 
-					"<key><time>%llu</time><type>%u</type><keycode>%llu</keycode><unichar>%04x</unichar><ascii>%c</ascii></key>\n",
-					(unsigned long long)	currentTime,
-					(unsigned int)			type,
-					(long long)				keycode,
-					uc[0],
-					uc[0]);
-		
-	}
-	else { // disable rest
-		fprintf((FILE*)pLogFile, 
-				"<key><time>%llu</time><type>%u</type><keycode>%llu</keycode><unichar>%04x</unichar></key>\n",
-				(unsigned long long)		currentTime,
-				(unsigned int)			type,
-				(long long)				keycode,
-				uc[0]);
-	}
+	printToLog(&timeStamp, &type, &keycode, uc, &ucc, &flags, pLogFile);
 
 	return event; 
 }
@@ -109,7 +140,7 @@ void createEventListenerLoopSourceAndRun(FILE* pLogFile)
 	CFRunLoopSourceRef	runLoopSource; 
 	
 	// create event listener
-	eventTap = CGEventTapCreate(kCGSessionEventTap, 
+	eventTap = CGEventTapCreate(kCGHIDEventTap, 
 								kCGHeadInsertEventTap, 
 								0, 
 								kCGEventMaskForAllEvents, 
